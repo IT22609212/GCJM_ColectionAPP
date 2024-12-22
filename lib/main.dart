7d3630c3c58c77ab6a58032de5e4fd7852f5a115
Akmal Alavi <akmal.alavi@rootcode.io>
2024-12-22 15:32:21 +0530

    feat: initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:gcjm_collection_app/Common_Widgets/bottom_bar.dart';
 import 'package:gcjm_collection_app/Screens/dashboard.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
